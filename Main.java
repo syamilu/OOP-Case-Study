e2dfc419620596e4ddb5c4da92a24fd34793985b
syamilu <wanmuhammadsyamil@gmail.com>
2023-12-06 18:36:02 +0800

    Initial commit

---
+++ Main.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Voucher> voucherList = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Welcome to the BlueShark Voucher Management System!");
+        System.out.println("---------------------------------------------------");
+        do {
+
+            System.out.println("Please select an option:");
+            System.out.println("1. Generate Voucher(s)");
+            System.out.println("2. Redeem Voucher(s)");
+            System.out.println("3. Modify Voucher");
+            System.out.println("4. Delete Voucher");
+            System.out.println("5. View Voucher(s)");
+            System.out.println("6. Exit");
+            System.out.print("Your choice: ");
+            int choice = input.nextInt();
+
+            switch (choice) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    System.out.println("Thank you for using the BlueShark Voucher Management System!");
+                    input.close();
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid choice!");
+            }
+            System.out.println();
+        } while (true);
+
+    }
+}
