b43c4b666c3072971ce97b61eb30941495336eb1
syamilu <wanmuhammadsyamil@gmail.com>
2023-12-11 01:42:53 +0800

    :zap: view Voucher class added, and some test data

--- Main.java
+++ Main.java
@@ -4,12 +4,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         ArrayList<Voucher> voucherList = new ArrayList<>();
+        int choice = 0;
         Scanner input = new Scanner(System.in);
+        // test data
+        for (int i = 0; i < 10; i++) {
+            voucherList.add(new Voucher(Integer.toString(i)));
+            if (i == 3 || i == 5) {
+                voucherList.get(i).setIsRedeemed();
+            }
+        }
+        // end of test data
 
         System.out.println("Welcome to the BlueShark Voucher Management System!");
         System.out.println("---------------------------------------------------");
         do {
-
             System.out.println("Please select an option:");
             System.out.println("1. Generate Voucher(s)");
             System.out.println("2. Redeem Voucher(s)");
@@ -18,7 +26,13 @@ public class Main {
             System.out.println("5. View Voucher(s)");
             System.out.println("6. Exit");
             System.out.print("Your choice: ");
-            int choice = input.nextInt();
+            do {
+                if (input.hasNextInt()) {
+                    choice = input.nextInt();
+                } else {
+                    System.out.println("Invalid input. Please enter an integer.");
+                }
+            } while (choice < 1 || choice > 6);
 
             switch (choice) {
                 case 1:
@@ -30,17 +44,19 @@ public class Main {
                 case 4:
                     break;
                 case 5:
+                    ViewVoucher.setVoucherList(voucherList);
+                    ViewVoucher.displayOptions(voucherList);
                     break;
                 case 6:
                     System.out.println("Thank you for using the BlueShark Voucher Management System!");
-                    input.close();
-                    System.exit(0);
                     break;
                 default:
                     System.out.println("Invalid choice!");
             }
             System.out.println();
-        } while (true);
+            input.nextLine();
+        } while (choice != 6);
+        input.close();
 
     }
 }
